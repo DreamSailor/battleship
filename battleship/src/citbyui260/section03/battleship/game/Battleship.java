5da729a6c49a4ea08fef509886c7bd2214164df9
jeffry Simpson <jeffrysimpson@gmail.com>
2015-04-03 14:10:53 -0700

    Gui for main screen

--- battleship/src/citbyui260/section03/battleship/game/Battleship.java
+++ battleship/src/citbyui260/section03/battleship/game/Battleship.java
@@ -2,65 +2,70 @@ package citbyui260.section03.battleship.game;
 
 import citbyui260.section03.battleship.msgs.BattleshipError;
 import citbyui260.section03.battleship.view.Menu;
-
+import citbyui260.section03.battleship.frames.*;
 
 //import battleship.Game;
-
-
-
-
-
 /**
  *
  * @author jeremyrklein
  */
-
-
-public class Battleship {
-    
-    String welcome = 
-              "\n\t***********************************************************************"
-            + "\n\t* Prepare your fleet. A battle is about to begin!                     *"                            
+public class Battleship
+{
+    private static MainFrame mainFrame=null;
+    String welcome
+            = "\n\t***********************************************************************"
+            + "\n\t* Prepare your fleet. A battle is about to begin!                     *"
             + "\n\t* You will be playing against the computer. The object of the game    *"
             + "\n\t* is to find and destroy your opponent's Battleship before yours is   *"
-            + "\n\t* destroyed.                                                          *" 
+            + "\n\t* destroyed.                                                          *"
             + "\n\t*                                                                     *"
             + "\n\t* Good Luck!!!!                                                       *"
             + "\n\t***********************************************************************"
             + "\n";
 
-    
-    public Battleship() {
-        
+    public Battleship()
+    {
+
         //Emty Constructor
     }
-    
-    
-        
-    public static void main(String[] args) 
-    {   
-        try {
-        
-        Battleship battleship = new Battleship();
-        battleship.display();
-     
-        Menu mainMenu = new Menu();
-        mainMenu.getInput();
-            } catch(Throwable ex)
+
+    public static void main(String[] args)
+    {
+        Battleship battleship = null;
+        try
+        {
+            battleship = new Battleship();
+            //battleship = new Battleship();
+            //battleship.display();
+         /* Create and display the form */
+            java.awt.EventQueue.invokeLater(new Runnable()
             {
+                @Override
+                public void run()
+                {
+                    Battleship.mainFrame = new MainFrame();
+                    Battleship.mainFrame.setVisible(true);
+                }
+            });
+
+           // Menu mainMenu = new Menu();
+           // mainMenu.getInput();
+        } catch (Throwable ex)
+        {
             BattleshipError.displayLine("Unexpected error: " + ex.getMessage());
             BattleshipError.displayLine(ex.getStackTrace().toString());
-            }
-              finally
+        } finally
         {
-        BattleshipError.displayLine("Dust thou art, unto dust thou shalt become");
+            if(Battleship.mainFrame != null)
+                Battleship.mainFrame.dispose();
+            //BattleshipError.displayLine("Dust thou art, unto dust thou shalt become");
         }
     }
-    
-        private void display() {
-        System.out.println(this.welcome);
-        
-        
-    }
-    
-}   
+
+//    private void display()
+//    {
+//        System.out.println(this.welcome);
+//
+//    }
+
+}
